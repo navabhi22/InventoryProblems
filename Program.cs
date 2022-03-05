7abd7627e3703a6f814a998422866da46bd3bc48
navabhi22 <navabhi22@gmail.com>
2022-03-05 22:14:29 +0530

    Greeting Message for Inventory Management Problems

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace InventoryDataManagementDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Inventory Management Problems");
+        }
+    }
+}
