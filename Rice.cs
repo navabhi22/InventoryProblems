4cee6c0b2993dd05daefeb992447a0010e498dd6
navabhi22 <navabhi22@gmail.com>
2022-03-05 22:19:01 +0530

    Abhinav Added Rice.cs

---
+++ Rice.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    public class Rice
+    {
+        public List<TypesOfRice> typesofRice;
+        public List<TypesOfPulses> typesofPulses;
+        public List<TypesOfWheat> typesofWheat;
+    }
+
+    public class TypesOfRice
+    {
+        public string name;
+        public int weight;
+        public int Price;
+    }
+    public class TypesOfPulses
+    {
+        public string name;
+        public int weight;
+        public int Price;
+    }
+    public class TypesOfWheat
+    {
+        public string name;
+        public int weight;
+        public int Price;
+    }
+}
