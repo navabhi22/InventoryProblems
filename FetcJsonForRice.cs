6cef748383555bcdbe6f09cea0a81c836792a51e
navabhi22 <navabhi22@gmail.com>
2022-03-05 22:18:30 +0530

    Abhinav Added FetcJsonForRice.cs

---
+++ FetcJsonForRice.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    public class FetcJsonForRice
+    {
+        public Rice Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Rice>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Problem in reading the file");
+                    return null;
+                }
+            }
+        }
+    }
+}
