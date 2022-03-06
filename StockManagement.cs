d6bf8c819bbdcb16121b9a3a4fd241d75bbc021d
navabhi22 <navabhi22@gmail.com>
2022-03-06 20:54:43 +0530

    Abhinav Added StockManagement.cs

---
+++ StockManagement.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    public class StockManagement
+    {
+        public List<Stocks> StocksPortfolio;
+    }
+
+    public class Stocks
+    {
+        public string Name;
+        public int Qty;
+        public int Price;
+    }
+}
