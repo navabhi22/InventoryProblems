10cb535cf04871166ab6247263e886eeec0b3681
navabhi22 <navabhi22@gmail.com>
2022-03-06 21:20:30 +0530

    Abhinav Added Products.cs

---
+++ Products.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    public class Products
+    {
+        public List<Product1> ProductType1;
+        public List<Product2> ProductType2;
+        public List<Product3> ProductType3;
+    }
+
+    public class Product1
+    {
+        public string Name;
+        public int weight;
+        public int Price;
+    }
+    public class Product2
+    {
+        public string Name;
+        public int Qty;
+        public int Price;
+    }
+    public class Product3
+    {
+        public string Name;
+        public int Liter;
+        public int Price;
+    }
+}
