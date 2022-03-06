6a12da95f11d9b771b808422d5b0392c1ff95278
navabhi22 <navabhi22@gmail.com>
2022-03-06 20:55:57 +0530

    Abhinav Added FetchJsonForStocks.cs

---
+++ FetchJsonForStocks.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    internal class FetchJsonForStocks
+    {
+        public StockManagement Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<StockManagement>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Problem in reading the file");
+                    return null;
+                }
+            }
+        }
+    }
+
+}
