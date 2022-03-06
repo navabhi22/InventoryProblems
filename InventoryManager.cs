06dd594c63b3bf3d906cfcf6e0f207bfba8c1fc6
navabhi22 <navabhi22@gmail.com>
2022-03-06 21:21:05 +0530

    Abhinav Added InventoryManager.cs

---
+++ InventoryManager.cs
@@ -0,0 +1,30 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryDataManagementDemo
+{
+    internal class InventoryManager
+    {
+        public Products Read(string path)
+        {
+            using (StreamReader file = new StreamReader(path))
+            {
+                try
+                {
+                    string json = file.ReadToEnd();
+                    return JsonConvert.DeserializeObject<Products>(json);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Problem in reading the file");
+                    return null;
+                }
+            }
+        }
+
+    }
+}
